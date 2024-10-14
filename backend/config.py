0e1b7315644ad4af1d118b599f8c6b6afd25c748
Jonathan Kirkland <jkirkland@eighteenfiftynine.com>
2024-10-13 17:07:52 -0700

    changed from user to group id

--- backend/config.py
+++ backend/config.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+
 class Settings:
     SIGNAL_PHONE_NUMBER = os.getenv("SIGNAL_PHONE_NUMBER")
-    USER_PHONE_NUMBER = os.getenv("USER_PHONE_NUMBER")
+    SIGNAL_GROUP_ID = os.getenv("SIGNAL_GROUP_ID")
+
 
 settings = Settings()
-
